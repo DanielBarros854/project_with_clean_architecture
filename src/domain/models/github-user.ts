da65d1317a10b98b34d5e353e1a8704c27426a33
DanielBarros854 <91908602+DanielBarros854@users.noreply.github.com>
2022-01-28 18:59:22 -0300

    feat: githubUser model

---
+++ src/domain/models/github-user.ts
@@ -0,0 +1,11 @@
+export type IGithubUser = {
+  id: number
+  name: string
+  email: string
+  avatarUrl: string
+  publicRepos: number
+  company: string
+  login: string
+  bio: string
+  createAt: string
+}
