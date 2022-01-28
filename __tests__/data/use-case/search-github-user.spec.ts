3b26db09e550a71e8a0e936f482e4ed89bcdf297
DanielBarros854 <91908602+DanielBarros854@users.noreply.github.com>
2022-01-28 20:52:02 -0300

    test: ensure searchGithubUser return with correct values

--- __tests__/data/use-case/search-github-user.spec.ts
+++ __tests__/data/use-case/search-github-user.spec.ts
@@ -1,8 +1,25 @@
 import { SearchGithubUser } from '../../../src/data/use-case/search-github-user'
+import { IApiSearchGithubUser } from '../../../src/presentaton/contract/api-search-github-user'
+
+class ApiSearchGithubUserStub implements IApiSearchGithubUser {
+  async search (name: string): Promise<any> {
+    return {
+      id: 1,
+      name: 'any_name',
+      email: 'any_email',
+      avatarUrl: 'any_avatarUrl',
+      publicRepos: 1,
+      company: 'any_company',
+      login: 'any_login',
+      bio: 'any_bio',
+      createdAt: 'any_createdAt'
+    }
+  }
+}
 
 describe('Search GithubUser data', () => {
   it('should return with correct values', async () => {
-    const sut = new SearchGithubUser()
+    const sut = new SearchGithubUser(new ApiSearchGithubUserStub())
     const user = await sut.search({ name: 'any_login' })
 
     expect(user).toEqual({
