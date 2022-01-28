6387022eb1f36d1685e6d1a02216873c91080e41
DanielBarros854 <91908602+DanielBarros854@users.noreply.github.com>
2022-01-28 20:01:41 -0300

    test: ensure search github user controller returns with searchGithubUser data

--- __tests__/presentation/controllers/search-github-user.spec.ts
+++ __tests__/presentation/controllers/search-github-user.spec.ts
@@ -1,10 +1,28 @@
+import { IGithubUser } from '../../../src/domain/models/github-user'
+import { ISearchByGithubUser } from '../../../src/domain/use-case/search-by-github-user'
 import { SearchGithubUserController } from '../../../src/presentaton/controllers/search-github-user'
+
+class SearchGithubUserStub implements ISearchByGithubUser {
+  async search (data: { name: string }): Promise<IGithubUser> {
+    return {
+      id: 1,
+      name: 'any_name',
+      email: 'any_email',
+      avatarUrl: 'any_avatarUrl',
+      publicRepos: 1,
+      company: 'any_company',
+      login: data.name,
+      bio: 'any_bio',
+      createdAt: 'any_createdAt'
+    }
+  }
+}
 
 describe('search githubUser controller', () => {
   it('should return with correct values', async () => {
-    const sut = new SearchGithubUserController()
+    const sut = new SearchGithubUserController(new SearchGithubUserStub())
 
-    const user = await sut.search({ name: 'any_name' })
+    const user = await sut.search({ name: 'any_login' })
 
     expect(user).toEqual({
       id: 1,
