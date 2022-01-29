386ffd91ce7d76650d46f2cf7002d7d624452032
DanielBarros854 <91908602+DanielBarros854@users.noreply.github.com>
2022-01-28 21:04:02 -0300

    feat: githubUser infra

---
+++ src/infra/api/get-github-user.ts
@@ -0,0 +1,9 @@
+import axios from 'axios'
+import { IApiSearchGithubUser } from '../../presentaton/contract/api-search-github-user'
+
+export class GetGithubUser implements IApiSearchGithubUser {
+  async search (name: string): Promise<any> {
+    const response = await axios.get(`https://api.github.com/users/${name}`)
+    return response.data
+  }
+}
