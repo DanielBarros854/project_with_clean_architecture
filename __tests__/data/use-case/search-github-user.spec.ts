a7ee0cfdee688658908ec6243a280fdd4f197af4
DanielBarros854 <91908602+DanielBarros854@users.noreply.github.com>
2022-01-28 21:06:59 -0300

    test:  adjust to return expected data from api

--- __tests__/data/use-case/search-github-user.spec.ts
+++ __tests__/data/use-case/search-github-user.spec.ts
@@ -7,12 +7,12 @@ class ApiSearchGithubUserStub implements IApiSearchGithubUser {
       id: 1,
       name: 'any_name',
       email: 'any_email',
-      avatarUrl: 'any_avatarUrl',
-      publicRepos: 1,
+      avatar_url: 'any_avatarUrl',
+      public_repos: 1,
       company: 'any_company',
       login: 'any_login',
       bio: 'any_bio',
-      createdAt: 'any_createdAt'
+      created_at: 'any_createdAt'
     }
   }
 }
